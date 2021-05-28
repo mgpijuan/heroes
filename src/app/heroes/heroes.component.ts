f073c816a10878843126604c6bfa1c3ab2b58c9a
mgpijuan <mgpijuan@gmail.com>
2021-05-28 10:36:16 +0200

    Final servicios

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,33 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-
-
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   // Variables que vamos a usar
-  heroes?: Hero[];
   selectedHero?: Hero;
+  heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
-  ngOnInit(): {
-    this.getHeroes()
+  ngOnInit() {
+    this.getHeroes();
   }
-  
-// Metodos que voy a usar en el HTML
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
 
-getHeroes(): void {
-  //this.heroes = this.heroService.getHeroes();
-  this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-}
+  // Metodos que voy a usar en el HTML
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    //this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
